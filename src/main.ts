87878c3dc06f8ccae1048c7600cf722905653e1b
kaicardenas0618 <kcardenasit@gmail.com>
2026-06-19 14:09:04 +0900

    v1.2.1

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@
 import { trains } from './data/trains';
 
 const MOD_ID = 'chinese-trains';
-const MOD_VERSION = '1.0.0';
+const MOD_VERSION = 'v1.2.1';
 const TAG = '[Chinese Trains]';
 
 const api = window.SubwayBuilderAPI;
